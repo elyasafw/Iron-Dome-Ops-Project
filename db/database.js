37acaddbbcf23302d26d6515851987e7f6532779
elyasaf waisman <elyasafw1997@gmail.com>
2026-07-15 11:33:59 +0300

    created database file with pool

---
+++ db/database.js
@@ -0,0 +1,15 @@
+import env from "dotenv";
+import mysql2 from "mysql2/promise";
+
+env.config();
+
+const pool = mysql2.createPool({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    connectionLimit: 10,
+});
+
+export default pool;
